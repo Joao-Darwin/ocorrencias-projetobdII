21aded275ada6067957d4edff5f8ed1822ebae71
João Darwin <joaodarwin.ip22@gmail.com>
2023-10-10 14:08:14 -0300

    refactor: Adicionar tratamento de exceção nos controllers

--- backend/src/controllers/Ocorrencia/index.js
+++ backend/src/controllers/Ocorrencia/index.js
@@ -1,42 +1,67 @@
 const Ocorrencia = require('../../models/Ocorrencia/index');
 
 const saveOcorrencia = async (req, res) => {
-    const ocorrenciaToSave = req.body;
+    try {
+        const ocorrenciaToSave = req.body;
 
-    const ocorrenciaSaved = await Ocorrencia.create(ocorrenciaToSave);
+        const ocorrenciaSaved = await Ocorrencia.create(ocorrenciaToSave);
 
-    res.send(ocorrenciaSaved);
+        res.send(ocorrenciaSaved);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 }
 
 const findAllOcorrencias = async (req, res) => {
-    const allOcorrencias = await Ocorrencia.findAll();
+    try {
+        const allOcorrencias = await Ocorrencia.findAll();
 
-    res.send(allOcorrencias);
+        res.send(allOcorrencias);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 }
 
 const findOcorrenciaById = async (req, res) => {
-    const idOcorrencia = req.params.id;
+    try {
+        const idOcorrencia = req.params.id;
 
-    const ocorrencia = await Ocorrencia.findOne({where: {id: idOcorrencia}});
+        const ocorrencia = await Ocorrencia.findOne({ where: { id: idOcorrencia } });
 
-    res.send(ocorrencia);
+        res.send(ocorrencia);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 }
 
 const updateOcorrencia = async (req, res) => {
-    const idOcorrencia = req.params.id;
-    const ocorrenciaUpdated = req.body;
+    try {
+        const idOcorrencia = req.params.id;
+        const ocorrenciaUpdated = req.body;
 
-    await Ocorrencia.update(ocorrenciaUpdated, {where: {id: idOcorrencia}});
+        await Ocorrencia.update(ocorrenciaUpdated, { where: { id: idOcorrencia } });
 
-    res.send(ocorrenciaUpdated);
+        res.send(ocorrenciaUpdated);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 };
 
 const removeOcorrencia = async (req, res) => {
-    const idOcorrencia = req.params.id;
+    try {
+        const idOcorrencia = req.params.id;
 
-    await Ocorrencia.destroy({where: {id: idOcorrencia}});
+        await Ocorrencia.destroy({ where: { id: idOcorrencia } });
 
-    res.send(`Ocorrencia de ID #${idOcorrencia} removido`);
+        res.send(`Ocorrencia de ID #${idOcorrencia} removido`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 }
 
 module.exports = {
