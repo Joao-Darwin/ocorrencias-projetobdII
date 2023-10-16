76db34447859207c984bbd4c594045b0cf278900
JoaoPedroli <joaopedrocz12345@gmail.com>
2023-10-16 20:48:26 -0300

    get ocurrences & beta: add ocurrences

--- frontend/index.js
+++ frontend/index.js
@@ -1,60 +1,200 @@
 let map, infoWindow;
+let clickLat, clickLng;
+const elModal = document.querySelector(".modal");
+const elBtnCancel = document.querySelector(".btn-cancel");
+const elBtnSubmit = document.querySelector(".btn-submit");
+const elIptTitle = document.querySelector(".ipt-title");
+const elIptType = document.querySelector(".ipt-type");
+const elIptDateTime = document.querySelector(".ipt-datetime");
+
+elBtnCancel.addEventListener("click", () => {
+  elModal.style.display = "none";
+});
+
+elBtnSubmit.addEventListener("click", () => {
+  post_ocurrence();
+});
+
+async function post_ocurrence() {
+  const datetimeValue = new Date(elIptDateTime.value);
+
+  const ocurrenceData = {
+    lat: clickLat,
+    lng: clickLng,
+    titulo: elIptTitle.value,
+    tipo: elIptType.value,
+    data: datetimeValue,
+  };
+
+  console.log(ocurrenceData);
+
+  const options = {
+    method: "POST",
+    Headers: {
+      Accept: "application.json",
+      "Content-Type": "application/json",
+    },
+    body: ocurrenceData,
+    Cache: "default",
+  };
+
+  const res = await fetch("http://10.3.21.185:3000/ocorrencias", options);
+}
 
 async function initMap() {
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
 
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -6.889844787676005, lng: -38.55889240586281 },
-        zoom: 14
-    });
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -6.889844787676005, lng: -38.55889240586281 },
+    zoom: 14,
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+  });
 
-    google.maps.event.addListener(map, "click", (event) => {
-        addMarker(event.latLng, map);
-    });
+  google.maps.event.addListener(map, "click", (event) => {
+    elModal.style.display = "flex";
+    (clickLat = event.latLng.lat()), (clickLng = event.latLng.lng());
+    resetFields();
+    addMarker(event.latLng.lat(), event.latLng.lng());
 
-    infoWindow = new google.maps.InfoWindow();
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-
-                infoWindow.setPosition(pos);
-                infoWindow.setContent("Location found.");
-                infoWindow.open(map);
-                map.setCenter(pos);
-            },
-            () => {
-                handleLocationError(true, infoWindow, map.getCenter());
-            }
-        );
+    function resetFields() {
+      elIptTitle.value = "";
+      elIptType.value = "";
+      elIptDateTime.value = "";
     }
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  }
+}
+
+async function addMarker(lat, lng) {
+  new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    map,
+    title: "Novo marcador",
+    animation: google.maps.Animation.DROP,
+    icon: "https://img.icons8.com/color/32/chess-com.png",
+  }).addListener("dblclick", () => {
+    alert(`Lat: ${lat} \nLng: ${lng}`);
+  });
+}
+
+async function getAndShowOcurrences() {
+  const res = await fetch("http://10.3.21.185:3000/ocorrencias").then((res) =>
+    res.json()
+  );
+  res.map((ocurrence) => {
+    const coord = ocurrence.localizacaoGeografica.coordinates;
+    addMarker(coord[0], coord[1]);
+  });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-        browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
-
-async function addMarker(location, map) {
-    marker = new google.maps.Marker({
-        position: { lat: location.lat(), lng: location.lng() },
-        map: map,
-        title: "Novo marcador",
-        animation: google.maps.Animation.DROP,
-        icon: "https://img.icons8.com/color/32/chess-com.png"
-    }).addListener('dblclick', () => {
-        alert(`Lat: ${location.lat()} \nLng: ${location.lng()}`)
-    });
-}
-
 
 initMap();
+getAndShowOcurrences();
